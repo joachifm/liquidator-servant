481858f612f3108634ed499ea2cefd21b6970f7b
Joachim Fasting <joachifm@fastmail.fm>
2019-07-09 10:21:56 +0200

    Api: init

---
+++ Api.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Api where
+
+import Control.Lens hiding (Strict)
+import qualified Data.Aeson as Aeson
+
+import Data.Swagger
+import Servant
+import Servant.Swagger
+
+import Schema
+
+------------------------------------------------------------------------
+-- Transaction
+------------------------------------------------------------------------
+
+type GetTransactionById
+  =  "transaction"
+  :> QueryParam' '[Required, Strict] "id" Int64
+  :> QueryParam' '[Required, Strict] "company_id" Int64
+  :> Get '[JSON] Transaction
+
+type AddTransaction
+  = "transaction"
+  :> ReqBody '[JSON] Transaction :> Post '[JSON] Transaction
+
+type TransactionApi
+  =    GetTransactionById
+  :<|> AddTransaction
+
+------------------------------------------------------------------------
+-- Liquidator
+------------------------------------------------------------------------
+
+type LiquidatorApi = TransactionApi
+
+liquidatorApi :: Proxy LiquidatorApi
+liquidatorApi = Proxy
+
+------------------------------------------------------------------------
+-- Swagger
+------------------------------------------------------------------------
+
+type SwaggerApi = "swagger.json" :> Get '[JSON] Swagger
+
+swaggerDoc :: Swagger
+swaggerDoc = toSwagger liquidatorApi
+  & info.title       .~ "liquidator"
+  & info.version     .~ "1.0"
+  & info.description ?~ "liquidator API"
+  & info.license     ?~ ("MIT" & url ?~ URL "https://opensource.org/licenses/MIT")
+
+------------------------------------------------------------------------
+-- Toplevel
+------------------------------------------------------------------------
+
+type Api = LiquidatorApi :<|> SwaggerApi
+
+api :: Proxy Api
+api = Proxy
+
+handler = undefined
+
+app = serve api handler
