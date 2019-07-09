3dbeef248e2c8967243dac86725f6ac547a73a7c
Joachim Fasting <joachifm@fastmail.fm>
2019-07-09 10:21:46 +0200

    shell.nix: init

---
+++ shell.nix
@@ -0,0 +1,25 @@
+with (import <nixpkgs>{});
+
+let
+  hsDevEnv = haskellPackages.ghcWithPackages (hsPkgs: with hsPkgs; [
+    async
+    text
+
+    aeson
+    cassava
+    lucid
+
+    servant
+    servant-lucid
+    servant-server
+    servant-swagger
+
+    wai
+    warp
+  ]);
+in
+
+mkShell {
+  name = "dev-shell";
+  buildInputs = [ hsDevEnv ];
+}
