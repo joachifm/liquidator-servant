2835c119a14011a0f295bce0da3f116f9d198d8a
Joachim Fasting <joachifm@fastmail.fm>
2019-07-16 08:37:43 +0200

    liquidator-client: add remaining transaction api methods

--- liquidator-client/src/Liquidator/Client.hs
+++ liquidator-client/src/Liquidator/Client.hs
@@ -13,11 +13,22 @@ getTransactionById :: Int64 -> Int64 -> ClientM Transaction
 addTransaction :: Transaction -> ClientM Transaction
 updateTransaction :: Transaction -> ClientM Transaction
 deleteTransaction :: Int64 -> Int64 -> ClientM NoContent
+insertTransactionArray :: [Transaction] -> ClientM [Transaction]
+getTransactionsByDate :: Int64 -> Text -> ClientM [Transaction]
+getTransactionsByDateRange :: Int64 -> Text -> Text -> ClientM [Transaction]
+getAllIncomeTransactions :: Int64 -> ClientM [Transaction]
+getAllExpenseTransactions :: Int64 -> ClientM [Transaction]
 getBalanceByDate :: Int64 -> Text -> ClientM Balance
 _ :<|> (      getTransactionById
          :<|> addTransaction
          :<|> updateTransaction
          :<|> deleteTransaction
+         :<|> insertTransactionArray
+         :<|> getAllTransaction
+         :<|> getTransactionsByDate
+         :<|> getTransactionsByDateRange
+         :<|> getAllIncomeTransactions
+         :<|> getAllExpenseTransactions
        )
   :<|> (      getBalanceByDate
        )
